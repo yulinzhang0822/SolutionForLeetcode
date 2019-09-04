c11e746bfa162ac2c89ed291e685b3aa19df5306
Yulin(Leonard) Zhang <yuliz12@uci.edu>
2019-09-04 11:18:53 +0800

    Create Solution_3.java

---
+++ 662/Solution_3.java
@@ -0,0 +1,18 @@
+class Solution {
+    int ans;
+    Map<Integer, Integer> left;
+    public int widthOfBinaryTree(TreeNode root) {
+        ans = 0;
+        left = new HashMap();
+        dfs(root, 0, 0);
+        return ans;
+    }
+    public void dfs(TreeNode root, int depth, int pos) {
+        if (root == null) return;
+        left.computeIfAbsent(depth, x-> pos);
+        ans = Math.max(ans, pos - left.get(depth) + 1);
+        dfs(root.left, depth + 1, 2 * pos);
+        dfs(root.right, depth + 1, 2 * pos + 1);
+    }
+}
+//Time:O(N), Space:O(N)
