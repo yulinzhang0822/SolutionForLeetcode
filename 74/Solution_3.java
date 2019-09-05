7b08348d8bc64b2b0194c35b8005b20bdad8960f
Yulin(Leonard) Zhang <yuliz12@uci.edu>
2019-09-05 18:16:24 +0800

    Create Solution_3.java

---
+++ 74/Solution_3.java
@@ -0,0 +1,27 @@
+//Use binary search.
+
+//n * m matrix convert to an array => matrix[x][y] => a[x * m + y]
+
+//an array convert to n * m matrix => a[x] =>matrix[x / m][x % m];
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+        int start = 0, rows = matrix.length, cols = matrix[0].length;
+        int end = rows * cols - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (matrix[mid / cols][mid % cols] == target) {
+                return true;
+            } 
+            if (matrix[mid / cols][mid % cols] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return false;
+    }
+}
